5d9acb8553bf8376f9b0077d96a9c1b1d524244a
abdelhakim tamoum <tamoum.hakim@gmail.com>
2022-01-29 13:37:04 -0500

    esseyage mouve landing page

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 // landing-page
+
 let landingPage = document.querySelector(".landing-page");
 let imagePage = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg"];
 setInterval( ()=> {
     let randomNbr = Math.floor(Math.random()* imagePage.length);
-    landingPage.style.cssText= `
+    /*landingPage.style.cssText= `
     background-image: url('../img/${imagePage[randomNbr]}'); 
-    transition: all .9s ease-in-out`
+    transition: all .9s ease-in-out`*/
+    landingPage.style.backgroundImage = `url('../img/${imagePage[randomNbr]}')`; 
 }, 3000);
 
 // setting-box
+
 let sittingBox = document.querySelector(".sitting-box");
 let sittingIcon = document.querySelector(".toggle-cog .fa-cog");
 sittingIcon.onclick = function(){
@@ -17,11 +20,13 @@ sittingIcon.onclick = function(){
 };
 
 // switch colors
+
 const colorsList = document.querySelectorAll(".colors-list li");
+
 colorsList.forEach(li => {
     
     li.addEventListener("click", (e) => {
-        console.log(e.target.dataset.color);
         document.documentElement.style.setProperty('--main-color', e.target.dataset.color);
     });
 });
+
