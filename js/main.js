f91f0a827c3e0354051740f10aa4ca6b8f43d35b
abdelhakim tamoum <tamoum.hakim@gmail.com>
2022-02-03 19:34:34 -0500

    modifier skills avec js

--- js/main.js
+++ js/main.js
@@ -75,3 +75,18 @@ random.forEach(button => { // forEach = loop
     });
 });
 };
+
+/* 8 - select skills  ------ */
+let ourSkills = document.querySelector(".skills");
+window.onscroll = function() {
+    let skillsOffsetTop = ourSkills.offsetTop;
+    let skillsOuterHeight = ourSkills.offsetHeight;
+    let windowHeight = this.innerHeight;
+    let windowScrollTop = this.pageYOffset;
+    if ( windowScrollTop > ( skillsOffsetTop + skillsOuterHeight - windowHeight )){
+        let allskills = document.querySelectorAll(".skills-box .skill-progress span");
+        allskills.forEach( skill => {
+            skill.style.width = skill.dataset.progress;
+        });
+    }
+};
